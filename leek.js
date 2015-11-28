68904ad86652dc6653f95f95dac29a9412f54c1c
mklj <michael.j2n@gmail.com>
2015-11-28 16:45:38 +0100

    bought a shotgun !

--- leek.js
+++ leek.js
@@ -1,64 +1,55 @@
 global WARCRY = "I bring havoc !";
-global RANGE_PISTOL = 7;
-global WEAPONS = [WEAPON_PISTOL, CHIP_SPARK];
 
-// On prend le pistolet
-//setWeapon(WEAPON_PISTOL); // Attention : coûte 1 PT
-
+if (! getWeapon())
+    setWeapon(WEAPON_SHOTGUN);
 // On récupère l'ennemi le plus proche
 var enemy = getNearestEnemy();
 // On avance vers l'ennemi
 moveToward(enemy);
-//var weapon = hasInView(enemy) ? WEAPON_PISTOL : 
+
 var shot_status = 1;
-if (hasInView(enemy))
+if (lineOfSight(getCell(), getCell(enemy)))
 {
-    setWeapon(WEAPON_PISTOL);
-    while (shot_status >= 0)
-    {
-        shot_status = useWeapon(enemy);
-    }
+    fireWeapon(enemy);
 }
 else
 {
-    while (shot_status >= 0)
-    {
-        shot_status = useChip(CHIP_SPARK, enemy);
-    }
+    fireSpark(enemy);
 }
 
 if (isDead(enemy))
 	say(WARCRY);
 
 //
-function moveInRange(cell, range)
+function fireWeapon(enemy)
 {
-    return false;    
+    var enemyCell = getCell(enemy);
+    var myCell = getCell();
+    var distance = getDistance(enemyCell, myCell);
+
+    if (distance <= getWeaponMaxRange(WEAPON_SHOTGUN) && isOnSameLine(enemyCell, myCell))
+    {
+        if (! getWeapon())
+            setWeapon(WEAPON_SHOTGUN);
+    }
+    else if (distance <= getWeaponMaxRange(WEAPON_PISTOL))
+    {
+        setWeapon(WEAPON_PISTOL);
+    }
+
+    var shot_status = 1;
+    while (shot_status >= 0)
+    {
+        shot_status = useWeapon(enemy);
+    }
 }
 
-// get highest range among availble weapons
-function bestRangeWeapon()
+function fireSpark(enemy)
 {
-	var weapons = getWeapons();
-	var best_range_weap = weapons[0];
-	for (var i = 1, var c = weapons.length; i < c; i++)
-	{
-		if (weapons[i].getWeaponMaxScope() > best_range_weap.getWeaponMaxScope())
-			best_range_weap = weapons[i];
-	}
-	return best_range_weap;
+    var shot_status = 1;
+    while (shot_status >= 0)
+    {
+        shot_status = useChip(CHIP_SPARK, enemy);
+    }
 }
 
-/*
- * Détermine si le poireau peut tirer sur l'ennemi d'id leek avec son arme
- * courante
- */
-function canUseWeapon(leek)
-{
-    return enemyInRange && false;
-}
-
-function hasInView(leek)
-{
-    return lineOfSight(getCell(), getCell(leek));
-}
